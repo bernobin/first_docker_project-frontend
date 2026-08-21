7eb09d4b82cddaf20de8115acd09d75b0a0fb531
berno <berno.binkert@gmail.com>
2026-08-21 23:11:27 +0200

    Initial frontend commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+async function sayHello() {
+  const response = await fetch('/api/hello', {
+    method: 'POST',
+  });
+
+  const data = await response.json();
+
+  console.log(data);
+}
+</script>
+
+<template>
+  <main>
+    <h1>Hello Docker</h1>
+
+    <button @click="sayHello">Hello</button>
+  </main>
+</template>
