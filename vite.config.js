c7858eb9e2bf70f6e0629c4d9fdc7625f2ac6d3f
berno <berno.binkert@gmail.com>
2026-08-29 18:50:56 +0200

    included polling for hot reloads in browser on host system

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
+    // watch enables hot reload outside the container
+    watch: {
+      usePolling: true,
+    },
+    // proxy forwards requests to the backend through the internal network
     proxy: {
       '/api': {
         target: 'http://backend:8000',
